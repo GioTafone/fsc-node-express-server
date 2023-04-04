9b81fdfd758ff63b89d8c51378b93d397ee0fa59
Giovanni Tafone <giovanni.tafone@gmail.com>
2023-04-04 17:25:42 +0300

    morgan setup

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ const generateId = () => {
   return maxId + 1;
 };
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+app.use(requestLogger);
+
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
@@ -86,6 +95,12 @@ app.delete("/api/notes/:id", (request, response) => {
 
   response.status(204).end();
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
