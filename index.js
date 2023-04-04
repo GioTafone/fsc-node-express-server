fc37c074b5a1c35078561ac3275e38ece0581ed9
Giovanni Tafone <giovanni.tafone@gmail.com>
2023-04-04 11:52:19 +0300

    CRUD implemented

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -26,16 +28,49 @@ app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
+app.post("/api/notes", (request, response) => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+
+  const note = request.body;
+  note.id = maxId + 1;
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
   if (note) {
-    response.json(note)
+    response.json(note);
   } else {
-    response.status(404).end()
+    response.status(404).send("Bad Request").end();
   }
-})
+});
+
+app.put("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const noteToUpdate = notes.find((note) => note.id === id);
+
+  if (!noteToUpdate) {
+    response.status(404).send("Note not found").end();
+    return;
+  }
+
+  const updatedNote = request.body;
+  notes = notes.map((note) => (note.id === id ? updatedNote : note));
+
+  response.json(updatedNote);
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
