bdc604f796bb02fac99ae8dfacb49da7ee090a9a
Giovanni Tafone <giovanni.tafone@gmail.com>
2023-04-16 15:32:50 +0300

    bug fix

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
