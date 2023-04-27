0b8d96a630576b55ed66a80028db2fd08dbd20c8
Giovanni Tafone <giovanni.tafone@gmail.com>
2023-04-27 11:52:20 +0300

    change eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,19 +13,11 @@ module.exports = {
     'rules': {
         'indent': [
             'error',
-            4
+            2
         ],
         'linebreak-style': [
             'error',
             'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
         ],
         'eqeqeq': 'error',
         'no-trailing-spaces': 'error',
