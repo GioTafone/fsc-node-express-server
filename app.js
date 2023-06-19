d283c3dc7a27aaefd5930a9ad14de2a319b64a71
Giovanni Tafone <giovanni.tafone@gmail.com>
2023-06-19 08:51:57 +0300

    user controller

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const config = require('./utils/config')
 const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -24,8 +25,10 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
-
+app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
+
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
